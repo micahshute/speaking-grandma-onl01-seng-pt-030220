3c6b9a689dcfd24f635dbd15f94d05f2e861aac3
Micah Shute <micah.shute@gmail.com>
2020-03-04 23:28:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -21,7 +21,7 @@
 def speak_to_grandma(phrase)
   answer = "HUH?! SPEAK UP, SONNY!"
   if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938"
   else 
